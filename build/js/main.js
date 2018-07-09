e3b0d734546d6be5f71cfc5753cf6ce50e123ef9
arpban <arpit.bansal2010@gmail.com>
2018-07-09 22:34:35 +0530

    meta

---
+++ build/js/main.js
@@ -0,0 +1,55 @@
+"use strict";
+
+function goback() {
+  if (window.history.length == 1) {
+    window.location.href = '/';
+  } else {
+    window.history.back();
+  }
+}
+
+//----------------- CAROUSEL ------------------ //
+
+var slides = document.querySelectorAll(".slide");
+var activeSlide = 0;
+
+function advanceSlides(count) {
+  if (!count) {
+    count = 1;
+  }
+  activeSlide = activeSlide + count;
+  if (activeSlide == slides.length) {
+    activeSlide = 0;
+  }
+  if (activeSlide < 0) {
+    activeSlide = slides.length - 1;
+  }
+  showSlide(activeSlide);
+}
+
+function showSlide(slideIndex) {
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].classList.remove("active");
+    // btns[i].classList.remove("active");
+  }
+  // btns[slideIndex].classList.add("active");
+  slides[slideIndex].classList.add("active");
+}
+
+var fwdClick = function fwdClick(evt) {
+  if (intervalId) {
+    window.clearInterval(intervalId);
+  }
+  advanceSlides();
+};
+
+var backClick = function backClick(evt) {
+  if (intervalId) {
+    window.clearInterval(intervalId);
+  }
+  advanceSlides(-1);
+};
+//start slider
+var intervalId = window.setInterval(advanceSlides, 3000);
+
+// ----------- CAROUSEL ENDS HERE ----------------//
